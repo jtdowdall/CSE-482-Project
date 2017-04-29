81c4f9335da42622a950a4049757ddecf6dfb786
jtdowdall <jtdowdall@gmail.com>
2017-04-29 19:16:02 -0400

    Created two separate sentiment functions for easier testing.

--- twit_anal.py
+++ twit_anal.py
@@ -13,6 +13,7 @@ C_KEY = "TQbAsSoi0iwzv76iEVFNkCN1p"
 C_SECRET = "eKYicCDpprdLvCxr1TFxCFzQMGB98Wk6AJovTPdkr6w0NYUSyR"
 A_TOKEN_KEY = "174046629-KF7Try8Qt6MLdn2BKx1Mwq3ybhOXNel0UByogAXg"
 A_TOKEN_SECRET = "y1lFV6qMO2E6hvCZp3FzjO15IvpD1yK5oIKzvBo9Xp0bw"
+afinn = Afinn(emoticons=True)
 if len(sys.argv) < 2:
     print("Please enter keyword as commandline argument.")
     exit()
@@ -27,13 +28,15 @@ api = tweepy.API(auth)
 def sentiment(text):
     r = requests.post("http://text-processing.com/api/sentiment/", data={"text":text})
     prob = json.loads(r.text)["probability"]
-    return (prob['neg'], prob['neutral'], prob['pos'])
+    return int(prob['neg'] < ['pos'])
+
+def sentiment2(text):
+    return int(afinn.score(text) > 0)
 
 class MyListener(StreamListener):
     def __init__(self, time_limit=-1):
         self.start_time = time.time()
         self.limit = time_limit
-        self.afinn = Afinn(emoticons=True)
         super(MyListener, self).__init__()
 
     def on_data(self, data):
@@ -48,15 +51,8 @@ class MyListener(StreamListener):
             if split != -1:
                 text = text[split+2:]
             text = text.replace('#', '')
-            neg, neutral, pos = sentiment(text)
-            api = 0
-            if neg < pos:
-                api = 1
-            afinn = self.afinn.score(text)
-            if afinn > 0:
-                afinn = 1
-            else:
-                afinn = 0
+            api = sentiment(text)
+            afinn = sentiment2(text)
             print('''
 
 **************TWEET**************
